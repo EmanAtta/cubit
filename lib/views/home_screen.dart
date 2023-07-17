29ba79f8ed92e210fcc442b52e9a11986579f6c6
EmanAtta <119707214+EmanAtta@users.noreply.github.com>
2023-07-17 21:27:47 +0300

    cubit

---
+++ lib/views/home_screen.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'sceand_page.dart';
+
+class homepage2 extends StatelessWidget {
+  const homepage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
+    final TextEditingController emailController = TextEditingController();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'first page',
+            style: TextStyle(color: Color.fromARGB(255, 53, 188, 197)),
+          ),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 73, 73, 126),
+        ),
+        body: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      label: Text('email'),
+                    ),
+                    validator: (value) {
+                      if (value!.contains("a")) {
+                        return null;
+                      } else {
+                        return "not vaild";
+                      }
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      label: Text('password'),
+                    ),
+                    validator: (value) {
+                      if (value!.length < 6) {
+                        return "password should be more than 6";
+                      }
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: 100,
+                  height: 30,
+                  color: Color.fromARGB(255, 75, 103, 129),
+                  child: MaterialButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        saveEmail(emailController.text);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  secondpage(email: emailController.text)),
+                        );
+                      }
+                    },
+                    child: Text(
+                      'login',
+                      style: TextStyle(color: Color.fromARGB(255, 5, 133, 192)),
+                    ),
+                  ),
+                )
+              ],
+            )));
+  }
+
+  saveEmail(String email) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("email", email);
+  }
+}
+
+
+
+
+/*backgroundColor: const Color.fromARGB(255, 61, 142, 153),
+      body: SafeArea(
+        child: Container(
+          child: Text('eman atta',
+          style: TextStyle(
+            fontSize: 20,
+        
+          ),),
+          color: const Color.fromARGB(255, 240, 228, 228),
+          margin: EdgeInsets.all(25),
+          width: 90,
+          height: 80,
+          
+          
+        ),
+      ),
+       */
+
+/*
+backgroundColor: Color.fromARGB(255, 123, 144, 163),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 161, 166, 168),
+        title:Text('home page',
+        style: TextStyle(
+          color: Color.fromARGB(255, 108, 85, 194)
+        ),) ,
+      ),
+      body: Column(children: [
+        Text('hellow',
+        style: TextStyle(
+          backgroundColor: Color.fromARGB(255, 109, 98, 199),
+          color: Colors.black,
+          fontSize: 25,
+          
+        ),),
+
+        Container(
+          width:300,
+          height: 200,
+          padding: EdgeInsets.only(top: 120,left: 70),
+          child: Image.asset('assets/ff.png'),
+
+        )
+
+      ],),
+*/ 
